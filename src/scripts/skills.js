f8c7486375601d33e632ca25e52d6bd992d9f710
Alina Yakimova <komarovatz@gmail.com>
2021-07-03 13:29:20 +0300

    slider из секции ворк исправлен

--- src/scripts/skills.js
+++ src/scripts/skills.js
@@ -1,1 +1,45 @@
-console.log("skills module");
+import Vue from "vue";
+
+const skillsItem = {
+  props: ["skill"],
+  template: "#skills-item",
+  methods: {
+    drawPercentageCircle() {
+      const circle = this.$refs["colored-circle"];
+      const dashArray = parseInt(
+        getComputedStyle(circle).getPropertyValue("stroke-dasharray")
+      );
+
+      const percent = (dashArray / 100) * (100 - this.skill.percent);
+
+      circle.style.strokeDashoffset = percent;
+    },
+  },
+  mounted() {
+    this.drawPercentageCircle();
+  },
+};
+
+const skillsRow = {
+  props: ["category"],
+  template: "#skills-row",
+  components: {
+    skillsItem
+  },
+};
+
+new Vue({
+  el: "#skills-component", // id компонента Vue в index.pug
+  template: "#skills-list", // id шаблона компонента skills.pug
+  components: {
+    skillsRow
+  },
+  data() { // записываем данные в объект data, чтобы обращаться к ним внутри компонента
+    return {
+      skills: []
+    };
+  },
+  created() { // запросить данные, не обращаясь к реальным дом-узлам
+    this.skills = require("../data/skills.json");
+  },
+});
