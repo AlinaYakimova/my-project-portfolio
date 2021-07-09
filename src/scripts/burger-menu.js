767df712c6ba7d55216c7e74d419c3f46b68e626
Alina Yakimova <komarovatz@gmail.com>
2021-07-09 20:48:58 +0300

    hamburger доделан

---
+++ src/scripts/burger-menu.js
@@ -0,0 +1,58 @@
+// import Vue from "vue";
+
+// new Vue({
+// el: "#hamburger-component",
+// template: "#hamburger-container",
+// data: {
+// changed: false
+// },
+// methods: {
+// changedClass() {
+// this.changed = !this.changed;
+// // this.$refs.body = !this.changed
+// }
+// }
+// });
+
+const body = document.querySelector("body");
+const button = document.querySelector(".hamburger");
+const menu = document.querySelector(".header__modal");
+const modalMenu = document.querySelector(".header__menu-modal");
+const modalButtons = document.querySelector(".header__buttons-modal");
+const content = document.querySelector(".hero__content");
+const headerButtons = document.querySelector(".header__buttons");
+const bg = document.querySelector(".welcome-section");
+
+function toggleMenu() {
+  body.classList.toggle('open-modal');
+  button.classList.toggle('is-active');
+  menu.classList.toggle('popup-menu--open');
+  modalMenu.classList.toggle('header__menu-modal--open');
+  modalButtons.classList.toggle('header__buttons-modal--open');
+  content.classList.toggle('hero__content--close');
+  headerButtons.classList.toggle('header__buttons--close');
+  bg.classList.toggle('welcome-section--bg-change');
+}
+
+button.addEventListener("click", toggleMenu);
+
+body.addEventListener("click", (e) => {
+  const target = e.target;
+  const dataValue = target.dataset.scrollTo;
+
+  if (target.classList.contains("hamburger--vortex")) {
+    toggleMenu()
+  } else if (target.classList.contains("menu__link")) {
+    scrollToSection(dataValue);
+    toggleMenu()
+  }
+});
+
+const scrollToSection = (attr) => {
+  const elem = document.querySelector(`[data-section-id=${attr}]`); console.log(elem.offsetTop)
+  window.scroll({
+    left: 0,
+    top: elem.offsetTop,
+    behavior: "smooth"
+  })
+};
